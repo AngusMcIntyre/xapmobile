98fcf7543a2f394161dad77d90f87a5965214693
gusdor@gmail.com <gusdor@gmail.com@161f9b42-0ef7-93c5-c1d7-1dc5e09decc2>
2013-01-18 10:58:00 +0000

    +Added version number to about screen =changed hashmap in AboutActivity.class to a sparsearray

--- trunk/src/primayer/android/delta/AboutActivity.java
+++ trunk/src/primayer/android/delta/AboutActivity.java
@@ -7,16 +7,21 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
-	HashMap<Integer, Uri> mLocationMap = new HashMap<Integer, Uri>();
+	SparseArray<Uri> mLocationMap = new SparseArray<Uri>();
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,18 @@ public class AboutActivity extends Activity {
         mLocationMap.put(R.id.france_viewonmap, Uri.parse("geo:45.794392,4.792204?q=45.794392,4.792204(Primayer+SAS)"));
         mLocationMap.put(R.id.malaysia_viewonmap, Uri.parse("geo:3.005029,101.536036?q=3.005029,101.536036(Primayer+Sdn+Bhd)"));
         
+        //update version number display
+        TextView versionView = (TextView)this.findViewById(R.id.textView_version);
+        String vName;
+		try {
+			vName = this.getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+			versionView.setText(versionView.getText() + " " + vName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			Log.w(this.getClass().toString(), "Error getting package version number");
+			e.printStackTrace();
+		}
+        		
         //check if mapping is avaliable
         if(!CheckMappingExists()){
         	View v = this.findViewById(R.id.uk_viewonmap);
@@ -72,15 +89,19 @@ public class AboutActivity extends Activity {
     public void onViewOnMapClicked(View view)
     {
     	Intent i = null;
-    	if(mLocationMap.containsKey(view.getId()))
-    		i = new Intent(Intent.ACTION_VIEW, mLocationMap.get(view.getId()));
+    	Uri uri = mLocationMap.get(view.getId(), null);
     	
-    	if(i != null){
-	    	try {
-				this.startActivity(i);
-			} catch (ActivityNotFoundException e) {
-				e.printStackTrace();
-			}
+    	if(uri != null)
+    	{  	
+	    	i = new Intent(Intent.ACTION_VIEW, mLocationMap.get(view.getId()));
+	    	
+	    	if(i != null){
+		    	try {
+					this.startActivity(i);
+				} catch (ActivityNotFoundException e) {
+					e.printStackTrace();
+				}
+	    	}
     	}
     }
 
